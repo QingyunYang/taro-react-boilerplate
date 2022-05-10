bbe860825d9a60a7514da3091c0da499ce356b47
yangqingyun <yangqingyun@hotmail.com>
2022-05-10 08:48:45 +0800

    feat: 项目配置：支持多端编译

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ const config = {
     828: 1.81 / 2,
   },
   sourceRoot: 'src',
-  outputRoot: 'dist',
+  outputRoot: `dist/${process.env.TARO_ENV}`,
   plugins: [],
   defineConstants: {},
   copy: {
