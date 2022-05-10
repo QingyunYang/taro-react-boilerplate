b3ed1b5d478b52918f7b3334d67fb9716bb3f707
yangqingyun <yangqingyun@hotmail.com>
2022-05-10 08:47:55 +0800

    feat: 入口页：优化界面

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,10 +4,12 @@ import './index.css'
 
 const Page: FC = () => (
   <View className="flex flex-col items-center justify-center h-screen">
-    <Text className="mt-1">Hello world!</Text>
-    <Text className="mt-2">Taro React Boilerplate</Text>
-    <Navigator className="mt-4 w-48 h-10 bg-gray-500 flex items-center justify-center text-light-500 rounded">
-      Start
+    <Text className="text-xl">Taro React Boilerplate</Text>
+    <Text className="mt-4">Hello world!</Text>
+    <Navigator className="mt-10">
+      <View className="w-48 h-10 bg-blue-500 flex items-center justify-center rounded-full text-white">
+        Start
+      </View>
     </Navigator>
   </View>
 )
