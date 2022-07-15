6b059f971ea9478f90288dc8af66adb39fbcfa10
yangqingyun <yangqingyun@hotmail.com>
2022-07-15 11:40:08 +0800

    style: style format

--- src/pages/404/App.tsx
+++ src/pages/404/App.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react'
 import { View, Text } from '@tarojs/components'
 
 export const App: FC = () => (
-  <View className="flex flex-col items-center justify-center h-screen">
+  <View className="flex h-screen flex-col items-center justify-center">
     <Text>Page Not Found</Text>
   </View>
 )
--- src/pages/antd-mobile/App.tsx
+++ src/pages/antd-mobile/App.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react'
 import './App.css'
 
 export const App: FC = () => (
-  <View className="flex flex-col items-center justify-center h-screen">
+  <View className="flex h-screen flex-col items-center justify-center">
     <Button color="primary">Button</Button>
   </View>
 )
