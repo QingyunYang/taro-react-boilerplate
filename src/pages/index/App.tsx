ed3be897484b5d62c43fadc17f00e7a407cc938a
yangqingyun <yangqingyun@hotmail.com>
2022-08-22 13:54:26 +0800

    feat: 首页添加 taro logo

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -1,9 +1,11 @@
 import type { FC } from 'react'
 import { View, Text, Navigator } from '@tarojs/components'
+import { TaroLogo } from './components/TaroLogo'
 
 export const App: FC = () => (
   <View className="flex h-screen flex-col items-center justify-center">
-    <Text className="text-xl">Taro React Boilerplate</Text>
+    <TaroLogo />
+    <Text className="mt-4 text-xl">Taro React Boilerplate</Text>
     <Text className="mt-4">Hello world!</Text>
     <Navigator className="mt-10" url="/pages/explorer/index">
       <View className="flex h-10 w-48 items-center justify-center rounded-full bg-blue-500 text-white">
---
+++ src/pages/index/components/TaroLogo/index.tsx
@@ -0,0 +1,5 @@
+import type { FC } from 'react'
+import { View } from '@tarojs/components'
+import './index.css'
+
+export const TaroLogo: FC = () => <View className="taro-logo h-10 w-10 rounded-md" />
