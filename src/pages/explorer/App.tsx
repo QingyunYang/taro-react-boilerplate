e2dc5b3dfadf2bf528afef93bda8e8b60c584421
yangqingyun <yangqingyun@hotmail.com>
2022-08-30 14:14:54 +0800

    feat: 添加集成 ossaui、ahooks 计划

--- src/pages/explorer/App.tsx
+++ src/pages/explorer/App.tsx
@@ -1,33 +1,42 @@
-import type { FC } from 'react'
 import { View } from '@tarojs/components'
+import type { FC } from 'react'
 import { SectionItem } from './components/SectionItem'
+
+const taroComponents = [
+  { url: '/sub-packages/taro-components/taroify/index', title: 'taroify' },
+  { url: '/sub-packages/taro-components/@antmjs+vantui/index', title: '@antmjs/vantui' },
+  { url: '/sub-packages/taro-components/ossaui/index', title: 'ossaui' },
+]
+
+const stateManagement = [
+  { url: '/sub-packages/state-management/redux/index', title: 'redux' },
+  { url: '/sub-packages/state-management/zustand/index', title: 'zustand' },
+  { url: '/sub-packages/state-management/recoil/index', title: 'recoil' },
+  { url: '/sub-packages/state-management/jotai/index', title: 'jotai' },
+]
+
+const h5Components = [
+  { url: '/sub-packages/h5-components/antd-mobile/index', title: 'antd-mobile' },
+]
+
+const hooks = [{ url: '/sub-packages/hooks/ahooks/index', title: 'ahooks' }]
 
 export const App: FC = () => (
   <View className="h-screen bg-gray-100 pr-3 pl-3 pt-2 pb-2">
     <SectionItem
-      links={[{ url: '/sub-packages/h5-components/antd-mobile/index', title: 'antd-mobile' }]}
-      title="H5 Components"
-      description="在 @tarojs/plugin-html 插件的支持下，可以一定程度上复用 web 端的组件"
-    />
-    <SectionItem
-      links={[
-        { url: '/sub-packages/taro-components/taroify/index', title: 'taroify' },
-        { url: '/sub-packages/taro-components/@antmjs+vantui/index', title: '@antmjs/vantui' },
-      ]}
+      links={taroComponents}
       title="Taro Components"
       description="基于 Taro 的跨端组件库"
     />
-    <SectionItem links={[]} title="Hooks" description="描述内容" />
+    <SectionItem links={stateManagement} title="State Management" description="流行的状态管理库" />
     <SectionItem
-      links={[
-        { url: '/sub-packages/state-management/redux/index', title: 'redux' },
-        { url: '/sub-packages/state-management/zustand/index', title: 'zustand' },
-        { url: '/sub-packages/state-management/recoil/index', title: 'recoil' },
-        { url: '/sub-packages/state-management/jotai/index', title: 'jotai' },
-      ]}
-      title="State Management"
-      description="流行的状态管理库"
+      links={h5Components}
+      title="H5 Components"
+      description="在 @tarojs/plugin-html 插件的支持下，可以一定程度上复用 web 端的组件"
     />
-    <SectionItem links={[]} title="Drag And Drop" description="描述内容" />
+    <SectionItem links={hooks} title="Hooks" description="可用的 react hook 库" />
+    <SectionItem links={[]} title="Drag And Drop" description="拖拽能力" />
+    <SectionItem links={[]} title="Animation" description="动画效果" />
+    <SectionItem links={[]} title="Game Engine" description="游戏引擎" />
   </View>
 )
