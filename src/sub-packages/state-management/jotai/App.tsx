2eaf3fc554680fab756998ef0841af561d92d4a6
yangqingyun <yangqingyun@hotmail.com>
2022-09-01 13:39:14 +0800

    feat: 集成 jotai 状态管理示例

--- src/sub-packages/state-management/jotai/App.tsx
+++ src/sub-packages/state-management/jotai/App.tsx
@@ -1,8 +1,24 @@
 import type { FC } from 'react'
-import { View, Text } from '@tarojs/components'
+import { atom, useAtom } from 'jotai'
+import { View, Text, Button } from '@tarojs/components'
 
-export const App: FC = () => (
-  <View className="flex h-screen flex-col items-center justify-center">
-    <Text>jotai</Text>
-  </View>
-)
+const counterAtom = atom(0)
+
+export const App: FC = () => {
+  const [counter, setCounter] = useAtom(counterAtom)
+
+  return (
+    <View className="flex h-screen flex-col items-center justify-center">
+      <Text>{counter}</Text>
+      <Button className="mt-2" onClick={() => setCounter((prev) => prev + 1)}>
+        +
+      </Button>
+      <Button className="mt-2" onClick={() => setCounter((prev) => prev - 1)}>
+        1
+      </Button>
+      <Button className="mt-2" onClick={() => setCounter((prev) => prev + 3)}>
+        +3
+      </Button>
+    </View>
+  )
+}
